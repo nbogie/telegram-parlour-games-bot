0661b6df5cc97444d0ad87b1c22447a4da0f23f5
Neill Bogie <neillbogie@googlemail.com>
2021-01-13 00:10:54 +0000

    add /dog for dog breed photo

--- index.js
+++ index.js
@@ -66,6 +66,38 @@ bot.command('photo', ({ replyWithPhoto }) => {
     replyWithPhoto({ url: randomPhotoURL })
 })
 
+bot.command('dog', async ctx => {
+    //1. get the breed from the command text
+    const parts = ctx.message.text.split(" ");
+    if (parts.length <= 1) {
+        return ctx.reply("Missing breed.  Try /dog spaniel")
+    }
+    const breed = parts[1];
+
+    2. //fetch a random image URL for this breed, from the dog API
+    const url = `https://dog.ceo/api/breed/${breed}/images/random`;
+
+    try {
+        const response = await axios.get(url);
+
+        //just an example response, unused
+        const exampleResponse = {
+            "message": "https://images.dog.ceo/breeds/hound-blood/n02088466_10335.jpg",
+            "status": "success"
+        }
+
+        const photoURL = response.data.message;
+
+        //3. reply with photo
+        ctx.replyWithPhoto(photoURL);
+    } catch (error) {
+        //TODO: this too-wide try-catch is hiding too 
+        //many possible errors unrelated to the fetch.
+        //Write a function that does the fetch and ALWAYS returns a value (Either Error RandomPhotoData)
+        ctx.reply("Error - maybe try a different breed?")
+    }
+})
+
 bot.command('fortune', ctx => {
     axios.get("http://yerkee.com/api/fortune")
         .then(function (response) {
